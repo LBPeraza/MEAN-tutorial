72b599646edc593ffc2596f4a710bea3e6441313
Lukas Peraza <lukasperaza@gmail.com>
2015-10-15 17:39:49 -0400

    remove bootstrap from node_modules and make basic page

---
+++ app.js
@@ -0,0 +1,29 @@
+(function() {
+    var app = angular.module('flapperNews', []);
+
+    app.controller('MainCtrl', ['$scope', function($scope) {
+        $scope.test = 'Hello world!';
+
+        $scope.posts = [];
+
+        $scope.addPost = function() {
+            if (!$scope.title || $scope.title === '')
+                return;
+
+            $scope.posts.push({
+                title: $scope.title,
+                link: $scope.link,
+                upvotes: 0,
+            });
+            
+            $scope.title = '';
+            $scope.link = '';
+        };
+
+        $scope.incrementUpvotes = function(post) {
+            post.upvotes ++;
+        }
+
+    }]);
+
+})();
